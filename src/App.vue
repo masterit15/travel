ba088567db9969f62ecb587fd931ce20f51f7c54
masterit15 <masterit15@yandex.ru>
2021-05-21 06:28:28 +0300

    sl

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,25 @@
         <div class="big_slider_list">
           <div v-for="(item, index) in items" :key="index" class="big_slider_item" :style="{backgroundImage: `url(${item.img})`}"></div>
         </div>
+        <div class="small_carousel">
+            <alignable-carousel :align="dynamicAlign">
+              <rl-carousel-slide v-for="(item, index) in items" :key="index">
+                          <div class="small_slider_item" :style="{backgroundImage: `url(${item.img})`}"></div>
+
+              </rl-carousel-slide>
+            </alignable-carousel>
+          <div class="flex justify-around">
+            <button @click="dynamicAlign = 'left'">
+              Left
+            </button>
+            <button @click="dynamicAlign = 'center'">
+              Centre
+            </button>
+            <button @click="dynamicAlign = 'right'">
+              Right
+            </button>
+          </div>
+        </div>
         <div class="big_slider_nav"></div>
         <div class="big_slider_counter"></div>
       </div>
@@ -16,33 +35,43 @@
   </div>
 </template>
 <script>
+import AlignableCarousel from './components/AlignableCarousel'
 export default {
-  data(){
+  components:{
+    AlignableCarousel
+  },
+  data() {
     return {
       items: [
         {
-          title: 'Test title slide 1',
-          text: 'Test exemplet zalupa text slide 1',
-          img: './assets/images/1.jpg'
+          title: "Test title slide 1",
+          text: "Test exemplet zalupa text slide 1",
+          img: "./assets/images/1.jpg",
         },
         {
-          title: 'Test title slide 2',
-          text: 'Test exemplet zalupa text slide 2',
-          img: './assets/images/2.jpg'
+          title: "Test title slide 2",
+          text: "Test exemplet zalupa text slide 2",
+          img: "./assets/images/2.jpg",
         },
         {
-          title: 'Test title slide 3',
-          text: 'Test exemplet zalupa text slide 3',
-          img: './assets/images/4.jpg'
-        },{
-          title: 'Test title slide 4',
-          text: 'Test exemplet zalupa text slide 4',
-          img: './assets/images/4.jpg'
-        }
-      ]
+          title: "Test title slide 3",
+          text: "Test exemplet zalupa text slide 3",
+          img: "./assets/images/4.jpg",
+        },
+        {
+          title: "Test title slide 4",
+          text: "Test exemplet zalupa text slide 4",
+          img: "./assets/images/4.jpg",
+        },
+      ],
+    };
+  },
+  methods: {
+    dynamicAlign(){
+      console.log('dynamicAlign');
     }
   }
-}
+};
 </script>
 <style lang="sass">
 *
@@ -87,4 +116,13 @@ body
   &_nav
     background: #000
     position: relative
+.small
+  &_carousel
+    position: relative
+    z-index: 110
+  &_slider
+    &_item
+      width: 300px
+      height: 400px
+      background-color: #333
 </style>
---
+++ src/components/AlignableCarousel.vue
@@ -0,0 +1,61 @@
+<template>
+  <rl-carousel
+    v-model="slide"
+    :align="align"
+  >
+    <div slot-scope="{ wrapperStyles, slides: { count, active } }">
+      <div class="overflow-hidden text-center">
+        <div v-bind="wrapperStyles">
+          <slot></slot>
+        </div>
+      </div>
+      <div class="mt-4 text-center">
+        <span
+          v-for="(item, index) in count"
+          :key="index"
+          class="mx-1 cursor-pointer"
+          @click="slide = index"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="30"
+            height="20"
+            class="stroke-current"
+            :class="{
+              'text-grey': active !== index,
+              'text-grey-dark': active === index
+            }"
+          >
+            <line x1="0" y1="10" x2="30" y2="10" stroke-width="2" />
+          </svg>
+        </span>
+      </div>
+    </div>
+  </rl-carousel>
+</template>
+
+<script>
+import { RlCarousel } from 'vue-renderless-carousel'
+export default {
+  name: 'alignable-carousel',
+  components: {
+    RlCarousel
+  },
+  props: {
+    align: {
+      type: String,
+      default: 'center'
+    }
+  },
+  data () {
+    return {
+      slide: 0
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+svg
+  transition: color .5s ease
+</style>
